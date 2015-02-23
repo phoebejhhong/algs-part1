53af54594e959d6cf899276a7eeaa61940ee0863
Phoebe Hong <phoebejhhong@gmail.com>
2015-02-22 20:25:27 -0800

    stacks

--- percolation/README.md
+++ percolation/README.md
@@ -9,7 +9,7 @@ http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
 ### The Problem
 * The problem. In a famous scientific problem, researchers are interested in the following question: if sites are independently set to be open with probability p (and therefore blocked with probability 1 − p), what is the probability that the system percolates? When p equals 0, the system does not percolate; when p equals 1, the system percolates. The plots below show the site vacancy probability p versus the percolation probability for 20-by-20 random grid (left) and 100-by-100 random grid (right).
 
-### Monte Carlo simulation.
+### Monte Carlo simulation
 * To estimate the percolation threshold, consider the following computational experiment:
 
  * Initialize all sites to be blocked.
---
+++ stack.rb
@@ -0,0 +1,105 @@
+# Algorithms, Part I, Week2, Stacks and Queues
+
+# Linked-list implementation of stack of strings
+class StackOfStrings
+
+  attr_accessor :first
+
+  def initialize
+    self.first = nil
+  end
+
+  def push(item)
+    oldfirst = first
+    self.first = Node.new
+    first.item = item
+    first.next = oldfirst
+  end
+
+  def pop
+    raise if is_empty?
+    item = first.item
+    self.first = first.next
+
+    item
+  end
+
+  def is_empty?
+    first.nil?
+  end
+
+  # inner class
+  class Node
+    attr_accessor :item, :next
+  end
+
+end
+
+# Fixed-capacity array implementation of stack of strings
+class FixedCapacityStackOfStrings
+  attr_accessor :stack, :idx
+
+  def initialize(capacity)
+    self.stack = Array.new(capacity)
+    self.idx = 0
+  end
+
+  def is_empty?
+    idx == 0
+  end
+
+  def push(item)
+    stack[idx] = item
+    self.idx += 1
+  end
+
+  def pop
+    raise if is_empty?
+    self.idx -= 1
+    item = stack[idx]
+    stack[idx] = nil
+
+    item
+  end
+end
+
+# Resizing array implementation of stack of strings
+class ResizingArrayStackOfStrings
+  attr_accessor :stack, :idx
+
+  def initialize
+    self.stack = Array.new(1)
+    self.idx = 0
+  end
+
+  def is_empty?
+    idx == 0
+  end
+
+  def push(item)
+    # double the size of array when full
+    resize(stack.length * 2) if idx == stack.length
+
+    stack[idx] = item
+    self.idx += 1
+  end
+
+  def pop
+    raise if is_empty?
+    self.idx -= 1
+    item = stack[idx]
+    stack[idx] = nil
+    # halve size of array when one-quater full
+    if idx > 0 && idx == (stack.length/4)
+      resize(stack.length / 2)
+    end
+
+    item
+  end
+
+  def resize(capacity)
+    copy = Array.new(capacity)
+    (0..capacity-1).each { |idx| copy[idx] = stack[idx] }
+    self.stack = copy
+  end
+end
